7ff886c1e3d0feda10026351b61b0c788ee92f38
sricharanreddy0007-vscr <sricharanreddy0007@gmail.com>
2026-07-30 09:30:10 +0000

    demo.java

---
+++ demo.java
@@ -0,0 +1,20 @@
+class Test{
+    int a,b;
+    Test()
+{
+    a=10;
+    b=20;
+    System.out.println("value of a "+a);
+    System.out.println("value of b"+b);
+
+}
+}
+class Testdemo
+{
+    public static void main(String[] args)
+    {
+    new Test();
+    //t1.Test();
+
+    }
+}
