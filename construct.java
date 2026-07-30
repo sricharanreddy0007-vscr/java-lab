75f6cfb0bfd341d600560bc2cd0c3684f72ba43b
sricharanreddy0007-vscr <sricharanreddy0007@gmail.com>
2026-07-30 09:53:55 +0000

    construct.java

---
+++ construct.java
@@ -0,0 +1,18 @@
+class Test{
+Test(int n1,int n2)
+{
+    System.out.println("the value of a is:"+n1);
+    System.out.println("the value of b is:"+n2);
+}
+}
+class construct
+ {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        new Test(a,b);
+
+    }
+
+} 
