27a0a3d57c8577658633a9999f278886e09efaa3
sricharanreddy0007-vscr <sricharanreddy0007@gmail.com>
2026-08-27 09:34:50 +0000

    initial.commit

---
+++ 27/08/2026/AnnonomusInner.java
@@ -0,0 +1,11 @@
+class Person {
+
+    void eat() {
+        System.out.println("Nice fruit");
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.eat();
+    }
+}
